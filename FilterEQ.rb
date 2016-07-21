0ade16ba2aacce2cf94cfc3794f2d55cf33f93c9
p2c2e <sudharsan.rangarajan@gmail.com>
2016-07-21 19:40:25 +0530

    Print better messages

--- FilterEQ.rb
+++ FilterEQ.rb
@@ -5,10 +5,11 @@
 #
 # Tk.getOpenFile
 #
+$stdout.sync = true
 
-rootPath="CHANGE THIS"
+rootPath="E:/H/Working/copy/"
 Dir.glob("#{rootPath}/*.csv") do |input_file|
-  puts "working on: #{input_file}..."
+  print "working on: #{input_file}..."
   bname = File.basename(input_file)
   output_file = "#{rootPath}/filtered/#{bname}"
   if not File.exists?(output_file)
@@ -17,6 +18,8 @@ Dir.glob("#{rootPath}/*.csv") do |input_file|
         ofile.puts(iline) if iline =~ /EQ/ or iline =~ /SERIES/
       end
     end
+    print "done\n"
+  else
+    print "exists\n"
   end
 end
-
